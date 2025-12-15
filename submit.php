0bb1ad4e6b10e4cf863cc6ad24825f002eab04e0
SteffyVinnyDaniel <steffy.23cs164@sode-edu.in>
2025-12-15 11:43:18 +0530

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+
+echo "<h1>Registration Successful</h1>";
+echo "<p>Name: $name</p>";
+echo "<p>Email: $email</p>";
+echo "<p>Phone: $phone</p>";
+?>
